facabcb290bac58c9e386de3335e84eed83248e4
sethbarton <sethwbarton@gmail.com>
2025-11-06 20:41:13 -0700

    FAIL: books#show should be the root path. Test to create new books and see it on show page.

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -1,8 +1,8 @@
 require "application_system_test_case"
 
 class BooksTest < ApplicationSystemTestCase
-  test "visiting the root page shows the new book form" do
-    visit root_path
+  test "visiting the new book page shows the new book form" do
+    visit new_book_path
     
     assert_selector "h1", text: "Add a New Book"
     assert_field "Title"
@@ -11,7 +11,7 @@ class BooksTest < ApplicationSystemTestCase
   end
 
   test "creating a new book" do
-    visit root_path
+    visit new_book_path
     
     fill_in "Title", with: "The Great Gatsby"
     fill_in "Author", with: "F. Scott Fitzgerald"
@@ -23,7 +23,7 @@ class BooksTest < ApplicationSystemTestCase
   end
 
   test "error messages are displayed when creating a book with invalid data" do
-    visit root_path
+    visit new_book_path
     
     # Try to submit form without filling in any fields
     click_button "Create Book"
@@ -35,6 +35,24 @@ class BooksTest < ApplicationSystemTestCase
     # Verify specific error messages for title and author
     assert_text "Title can't be blank"
     assert_text "Author can't be blank"
+  end
+
+  test "visiting the show page, creating a book, and seeing it on the show page" do
+    visit root_path
+    
+    # Click button to create a new book
+    click_button "Create Book"
+    
+    # Fill out the form
+    fill_in "Title", with: "The Great Gatsby"
+    fill_in "Author", with: "F. Scott Fitzgerald"
+    click_button "Create Book"
+    
+    # Should be redirected back to the show page (root)
+    assert_current_path root_path
+    # Assert that the book just created exists on the page
+    assert_text "The Great Gatsby"
+    assert_text "F. Scott Fitzgerald"
   end
 end
 
